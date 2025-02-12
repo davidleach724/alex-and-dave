9e3d3005a27ac614814769169b596eb01d6172f8
David Leach <davidleach724@gmail.com>
2025-02-11 20:09:30 -0700

    Add repeating image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import profilepic from './profilepic.jpeg';
 
-function App() {
+const App = () => {
+  const backgroundStyle = {
+    backgroundImage: `url(${profilepic})`,
+    backgroundRepeat: 'repeat',
+    backgroundSize: 'contain', // Ensures the full image is visible in each tile
+    backgroundPosition: 'top left',
+    width: '100vw',
+    height: '100vh',
+    position: 'absolute',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main style={backgroundStyle}>
+      <a href='https://frnkastro.bandcamp.com'></a>
+    </main>
   );
-}
+};
 
 export default App;
