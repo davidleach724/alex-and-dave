e096a576c5a65384f07eae7554ce8721112bd65c
David Leach <davidleach724@gmail.com>
2025-02-14 12:17:17 -0700

    update repeat

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
-import profilepic from './profilepic.jpeg';
+import "./App.css";
+import profilepic from "./profilepic.jpeg";
 
 const App = () => {
   const backgroundStyle = {
     backgroundImage: `url(${profilepic})`,
-    // backgroundRepeat: 'repeat',
-    backgroundSize: 'contain',
-    backgroundPosition: 'top left',
-    width: '100vw',
-    height: '100vh',
-    position: 'absolute',
+    backgroundRepeat: "repeat",
+    backgroundSize: "contain",
+    backgroundPosition: "top left",
+    width: "100vw",
+    height: "100vh",
+    position: "absolute",
   };
 
   return (
-    <main style={backgroundStyle}/>
+    <>
+      <main style={backgroundStyle} />
+      {console.log("test")}
+    </>
   );
 };
 
